1fa7b8fd000f3f9140ca3675fddba57ef438aef2
Maciej Małecki <me@mmalecki.com>
2015-04-19 22:25:35 -0700

    Implement the example server

--- examples/iojs-todo/deploy.js
+++ examples/iojs-todo/deploy.js
@@ -2,22 +2,12 @@ var async = require('async')
 var apt = require('nibbler-apt')
 var iojs = require('nibbler-debian-iojs')
 
-module.exports = function(context, cb) {
-  async.series([
-    async.apply(apt, {
-      updateCache: true,
-      state: 'present',
-      pkg: ['redis-server']
-    }, context),
-    async.apply(iojs, context)
-  ], cb)
-}
-
 module.exports = [
   [ apt, {
     updateCache: true,
     state: 'present',
     pkg: ['redis-server']
   } ],
-  iojs
+  iojs,
+  copyContext
 ]
